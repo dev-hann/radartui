4b0017d51acf6ea6415d7f6a8b70481299b2baa5
hann <developer.hann@gmail.com>
2025-05-04 18:17:11 +0900

    🎨 Feat: textfield

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,23 +1,42 @@
 import 'package:radartui/radartui.dart';
+import 'package:radartui/widget/expnaded.dart';
 import 'package:radartui/widget/focus_manager.dart';
 import 'package:radartui/widget/row.dart';
+import 'package:radartui/widget/text_field.dart';
 
 void main(List<String> arguments) {
   Radartui.runApp(
-    Card(
-      child: Row(
-        children: [
-          Card(
-            child: Card(child: ListView(items: ["1234567890", "234", "4567"])),
-          ),
-          Card(child: ListView(items: ["1234567890", "234", "4567"])),
-        ],
-      ),
+    Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Card(
+                child: Card(
+                  child: ListView(items: ["1234567890", "234", "4567"]),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 8,
+              child: Card(
+                child: Card(
+                  child: ListView(items: ["1234567890", "234", "4567"]),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Card(child: TextField()),
+      ],
     ),
     onKey: (key) {
       switch (key.label) {
         case "q":
-          Radartui.exitApp();
+          if (key.ctrl) {
+            Radartui.exitApp();
+          }
           break;
         case "\t": // 탭키 누르면
           FocusManager.instance.focusNext();
