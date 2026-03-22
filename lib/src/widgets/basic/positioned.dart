ff49fdea66212697c418b0121777efbc43b11e00
dev-hann <developer.hann@gmail.com>
2026-03-22 12:17:11 +0900

    feat: stack and positioned widgets

---
+++ lib/src/widgets/basic/positioned.dart
@@ -0,0 +1,67 @@
+import '../../../radartui.dart';
+
+class Positioned extends ParentDataWidget<StackParentData> {
+  final int? left;
+  final int? top;
+  final int? right;
+  final int? bottom;
+  final int? width;
+  final int? height;
+
+  const Positioned({
+    super.key,
+    this.left,
+    this.top,
+    this.right,
+    this.bottom,
+    this.width,
+    this.height,
+    required super.child,
+  });
+
+  const Positioned.fill({
+    super.key,
+    required Widget child,
+  }) : left = 0,
+       top = 0,
+       right = 0,
+       bottom = 0,
+       width = null,
+       height = null,
+       super(child: child);
+
+  @override
+  void applyParentData(RenderObject renderObject) {
+    final parentData = renderObject.parentData;
+    if (parentData is StackParentData) {
+      bool needsLayout = false;
+      if (parentData.left != left) {
+        parentData.left = left;
+        needsLayout = true;
+      }
+      if (parentData.top != top) {
+        parentData.top = top;
+        needsLayout = true;
+      }
+      if (parentData.right != right) {
+        parentData.right = right;
+        needsLayout = true;
+      }
+      if (parentData.bottom != bottom) {
+        parentData.bottom = bottom;
+        needsLayout = true;
+      }
+      if (parentData.width != width) {
+        parentData.width = width;
+        needsLayout = true;
+      }
+      if (parentData.height != height) {
+        parentData.height = height;
+        needsLayout = true;
+      }
+      if (needsLayout) {
+        renderObject.markNeedsLayout();
+      }
+    }
+  }
+}
--- lib/src/widgets/basic/stack.dart
+++ lib/src/widgets/basic/stack.dart
@@ -2,6 +2,7 @@ import '../../../radartui.dart';
 
 class Stack extends MultiChildRenderObjectWidget {
   const Stack({
+    super.key,
     required super.children,
   });
 
@@ -9,53 +10,113 @@ class Stack extends MultiChildRenderObjectWidget {
   RenderStack createRenderObject(BuildContext context) => RenderStack();
 
   @override
-  void updateRenderObject(BuildContext context, RenderObject renderObject) {
-    // No properties to update for basic Stack
-  }
+  void updateRenderObject(BuildContext context, RenderObject renderObject) {}
 }
 
 class StackParentData extends ParentData {
+  int? left;
+  int? top;
+  int? right;
+  int? bottom;
+  int? width;
+  int? height;
   Offset offset = Offset.zero;
 }
 
 class RenderStack extends RenderBox
     with ContainerRenderObjectMixin<RenderBox, StackParentData> {
+  @override
+  void setupParentData(RenderObject child) {
+    if (child.parentData is! StackParentData) {
+      child.parentData = StackParentData();
+    }
+  }
 
   @override
   void performLayout(Constraints constraints) {
-    final boxConstraints = constraints as BoxConstraints;
+    final boxConstraints = constraints.asBoxConstraints;
     int stackWidth = 0;
     int stackHeight = 0;
 
-    // Give children loose constraints so they can size themselves
     final childConstraints = boxConstraints.loosen();
 
-    // Layout all children and determine the stack size
     for (final child in children) {
-      // Setup parent data if needed
-      if (child.parentData is! StackParentData) {
-        child.parentData = StackParentData();
+      setupParentData(child);
+
+      final childParentData = child.parentData as StackParentData;
+
+      if (childParentData.width != null && childParentData.height != null) {
+        child.layout(BoxConstraints.tight(
+          Size(childParentData.width!, childParentData.height!),
+        ));
+      } else if (childParentData.width != null) {
+        child.layout(BoxConstraints(
+          minWidth: childParentData.width!,
+          maxWidth: childParentData.width!,
+          minHeight: 0,
+          maxHeight: boxConstraints.maxHeight,
+        ));
+      } else if (childParentData.height != null) {
+        child.layout(BoxConstraints(
+          minWidth: 0,
+          maxWidth: boxConstraints.maxWidth,
+          minHeight: childParentData.height!,
+          maxHeight: childParentData.height!,
+        ));
+      } else {
+        child.layout(childConstraints);
       }
 
-      child.layout(childConstraints);
       stackWidth = stackWidth > child.size!.width ? stackWidth : child.size!.width;
       stackHeight = stackHeight > child.size!.height ? stackHeight : child.size!.height;
-
-      // All children are positioned at (0, 0) for basic stacking
-      final childParentData = child.parentData as StackParentData;
-      childParentData.offset = const Offset(0, 0);
     }
 
-    // Stack takes the size from constraints, not children
-    size = boxConstraints.constrain(Size(
-      stackWidth > 0 ? stackWidth : boxConstraints.minWidth,
-      stackHeight > 0 ? stackHeight : boxConstraints.minHeight,
-    ));
+    final finalWidth = stackWidth > 0 ? stackWidth : boxConstraints.minWidth;
+    final finalHeight = stackHeight > 0 ? stackHeight : boxConstraints.minHeight;
+    size = boxConstraints.constrain(Size(finalWidth, finalHeight));
+
+    for (final child in children) {
+      final childParentData = child.parentData as StackParentData;
+      final childWidth = child.size!.width;
+      final childHeight = child.size!.height;
+
+      int left;
+      int top;
+
+      if (childParentData.left != null && childParentData.right != null) {
+        final width = finalWidth - childParentData.right! - childParentData.left!;
+        left = childParentData.left!;
+        if (width != childWidth) {
+          child.layout(BoxConstraints.tightFor(width: width, height: childHeight));
+        }
+      } else if (childParentData.left != null) {
+        left = childParentData.left!;
+      } else if (childParentData.right != null) {
+        left = (finalWidth - childParentData.right! - childWidth).clamp(0, finalWidth);
+      } else {
+        left = 0;
+      }
+
+      if (childParentData.top != null && childParentData.bottom != null) {
+        final height = finalHeight - childParentData.bottom! - childParentData.top!;
+        top = childParentData.top!;
+        if (height != childHeight) {
+          child.layout(BoxConstraints.tightFor(width: child.size!.width, height: height));
+        }
+      } else if (childParentData.top != null) {
+        top = childParentData.top!;
+      } else if (childParentData.bottom != null) {
+        top = (finalHeight - childParentData.bottom! - childHeight).clamp(0, finalHeight);
+      } else {
+        top = 0;
+      }
+
+      childParentData.offset = Offset(left, top);
+    }
   }
 
   @override
   void paint(PaintingContext context, Offset offset) {
-    // Paint children in order (first child at bottom, last child on top)
     for (final child in children) {
       final childParentData = child.parentData as StackParentData;
       context.paintChild(child, offset + childParentData.offset);
