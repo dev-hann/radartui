1e34ddc67e802902e79f839adf64fe6f8ae73e38
dev-hann <developer.hann@gmail.com>
2026-04-01 08:03:10 +0900

    feat: add Animation base classes and AnimationStatus

---
+++ lib/src/animation/animation.dart
@@ -0,0 +1,59 @@
+typedef VoidCallback = void Function();
+typedef AnimationStatusListener = void Function(AnimationStatus status);
+
+enum AnimationStatus {
+  dismissed,
+  forward,
+  reverse,
+  completed,
+}
+
+abstract class Animation<T> {
+  T get value;
+
+  AnimationStatus get status;
+
+  bool get isDismissed => status == AnimationStatus.dismissed;
+  bool get isCompleted => status == AnimationStatus.completed;
+
+  void addListener(VoidCallback listener);
+  void removeListener(VoidCallback listener);
+
+  void addStatusListener(AnimationStatusListener listener);
+  void removeStatusListener(AnimationStatusListener listener);
+}
+
+class AnimationListeners<T> {
+  final List<VoidCallback> _listeners = [];
+  final List<AnimationStatusListener> _statusListeners = [];
+
+  void addListener(VoidCallback listener) {
+    _listeners.add(listener);
+  }
+
+  void removeListener(VoidCallback listener) {
+    _listeners.remove(listener);
+  }
+
+  void addStatusListener(AnimationStatusListener listener) {
+    _statusListeners.add(listener);
+  }
+
+  void removeStatusListener(AnimationStatusListener listener) {
+    _statusListeners.remove(listener);
+  }
+
+  void notifyListeners() {
+    final listeners = List<VoidCallback>.from(_listeners);
+    for (final listener in listeners) {
+      listener();
+    }
+  }
+
+  void notifyStatusListeners(AnimationStatus status) {
+    final listeners = List<AnimationStatusListener>.from(_statusListeners);
+    for (final listener in listeners) {
+      listener(status);
+    }
+  }
+}
---
+++ test/unit/animation/animation_test.dart
@@ -0,0 +1,43 @@
+import 'package:radartui/src/animation/animation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Animation', () {
+    test('AnimationStatus enum has correct values', () {
+      expect(AnimationStatus.dismissed.index, equals(0));
+      expect(AnimationStatus.forward.index, equals(1));
+      expect(AnimationStatus.reverse.index, equals(2));
+      expect(AnimationStatus.completed.index, equals(3));
+    });
+  });
+
+  group('AnimationListeners', () {
+    test('addListener and removeListener work correctly', () {
+      final listeners = AnimationListeners<int>();
+      var callCount = 0;
+
+      void listener() => callCount++;
+
+      listeners.addListener(listener);
+      listeners.notifyListeners();
+      expect(callCount, equals(1));
+
+      listeners.removeListener(listener);
+      listeners.notifyListeners();
+      expect(callCount, equals(1));
+    });
+
+    test('multiple listeners are all called', () {
+      final listeners = AnimationListeners<int>();
+      var count1 = 0;
+      var count2 = 0;
+
+      listeners.addListener(() => count1++);
+      listeners.addListener(() => count2++);
+      listeners.notifyListeners();
+
+      expect(count1, equals(1));
+      expect(count2, equals(1));
+    });
+  });
+}
